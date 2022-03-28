a281232fd78d3e602ac1ffbbfda55c797454b0d5
Emiliya Vuntsova <vuntsova@aol.com>
2022-03-28 11:06:01 -0500

    Forgot Password functionality, invalid login message, enter key check

--- cypress/integration/DishwasherPurchase/LogIn.js
+++ cypress/integration/DishwasherPurchase/LogIn.js
@@ -1,7 +1,6 @@
 /// <reference types="cypress" />
 
 describe('Log In', () => {
-  
   beforeEach(() => {
     cy.visit('');
 
@@ -12,13 +11,13 @@ describe('Log In', () => {
         cy.get('.close').click();
       }
     });
-  });
-
-  //Verify if a user will be able to login with a valid username and valid password.
-  it('successfully logging in with a valid username and valid password', () => {
     cy.get(
       '.sign-in-account__link-log-in > .sign-in-account__icon > .icon > path'
     ).click();
+  });
+
+  //Verify if a user will be able to login with a valid username and valid password. Positive Tets.
+  it('Successfully logging in with a valid username and valid password', () => {
     cy.get('#headerSigninEmail').type(Cypress.env('emailAddress'));
     cy.get('#modalSigninPassword').type(Cypress.env('password'), {
       log: false,
@@ -29,11 +28,8 @@ describe('Log In', () => {
     cy.get('.breadcrumbs-list-item-last').should('be.visible');
   });
 
-  //Verify if a user cannot login with a valid username and an invalid password.
+  //Verify if a user cannot login with a valid username and an invalid password. Negative Test.
   it('Unsuccessful  logging atempt when a valid username and invalid password are provided', () => {
-    cy.get(
-      '.sign-in-account__link-log-in > .sign-in-account__icon > .icon > path'
-    ).click();
     cy.get('#headerSigninEmail').type(Cypress.env('emailAddress'));
     cy.get('#modalSigninPassword').type(Cypress.env('invalid_password'), {
       log: false,
@@ -42,15 +38,39 @@ describe('Log In', () => {
     cy.get('.form-submit-error').should('be.visible');
   });
 
-  //Verify the login page for both, when the field is blank and Submit button is clicked.
+  //Verify the login page for both, when the field is blank and Submit button is clicked. Negative Test.
   it('Unsuccessful logging atempt when the email field is blank and Submit button is clicked', () => {
-    cy.get(
-      '.sign-in-account__link-log-in > .sign-in-account__icon > .icon > path'
-    ).click();
-
-    cy.get('#modalSigninPassword').type(Cypress.env('password'), {log: false,});
+    cy.get('#modalSigninPassword').type(Cypress.env('password'), {
+      log: false,
+    });
 
     cy.get('#login-form > .form-buttons > .form-button').click();
     cy.get('.form-error-items.filled').should('be.visible');
+  });
+
+  //Verify the ‘Forgot Password’ functionality. Positive Test.
+  it('Checks "Forgot Password" functionality', () => {
+    cy.get('.login-forgot-password-link').click();
+    cy.url().should('include', '/forgot-password.html');
+  });
+
+  //Verify the messages for invalid login. Positive Test
+  it('Checks messages for invalid login', () => {
+    cy.get('#headerSigninEmail').type(Cypress.env('emailAddress'));
+    cy.get('#modalSigninPassword').type(Cypress.env('invalid_password'), {
+      log: false,
+    });
+    cy.get('#login-form > .form-buttons > .form-button').click();
+    cy.get('.form-submit-error').should('be.visible');
+  });
+
+  //Verify if the ‘Enter’ key of the keyboard is working correctly on the login page.	Positive Test
+  it.only('Checks if the "Enter" key of the keyboard is working', () => {
+    cy.get('#headerSigninEmail').type(Cypress.env('emailAddress'));
+    cy.get('#modalSigninPassword').type(Cypress.env('password'), {
+      log: false,
+    });
+    cy.get('#modalSigninPassword').type('{enter}');
+    cy.get('.breadcrumbs-list-item-last').should('be.visible');
   });
 });
